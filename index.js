99265c0572291ade33c9282e6d3489e62f7823de
redrumkumg <n.ronnapat@github.com>
2024-05-13 15:09:48 +0700

    apply

---
+++ index.js
@@ -0,0 +1,19 @@
+const dayNumber = 5;
+
+if (dayNumber === 0) {
+  console.log("Sunday");
+} else if (dayNumber === 1) {
+  console.log("Monday");
+} else if (dayNumber === 2) {
+  console.log("Tuesday");
+} else if (dayNumber === 3) {
+  console.log("Wednesday");
+} else if (dayNumber === 4) {
+  console.log("Thursday");
+} else if (dayNumber === 5) {
+  console.log("Friday");
+} else if (dayNumber === 6) {
+  console.log("Saturday");
+} else {
+  console.log("We only have seven days a week. Create your day");
+}
